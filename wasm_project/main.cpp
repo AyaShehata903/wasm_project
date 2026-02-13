213a1e77d0db0d4da767c37329cfc29a1529c8cf
lenovo <lenovo@DESKTOP-VDLEIH2>
2026-02-13 15:00:56 +0200

    Errors Fixed

--- wasm_project/main.cpp
+++ wasm_project/main.cpp
@@ -4,27 +4,27 @@
 extern "C" {
 
     char* encrypt(const char* input) {
-        string str(input);
+        std::string str(input);
 
         for(char &c : str) {
             c = c + 3;
         }
 
         char* result = new char[str.length() + 1];
-        strcpy(result, str.c_str());
+        std::strcpy(result, str.c_str());
 
         return result;
     }
 
     char* decrypt(const char* input) {
-        string str(input);
+        std::string str(input);
 
         for(char &c : str) {
             c = c - 3;
         }
 
         char* result = new char[str.length() + 1];
-        strcpy(result, str.c_str());
+        std::strcpy(result, str.c_str());
 
         return result;
     }
